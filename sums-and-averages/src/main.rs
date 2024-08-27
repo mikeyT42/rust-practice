bcb5097c6a24d26a8e61f0aad152f80b0d310bf5
Michael Torres <mrmojorisen0@gmail.com>
2024-08-27 15:55:51 -0400

    Starting the writing of the program.

--- sums-and-averages/src/main.rs
+++ sums-and-averages/src/main.rs
@@ -1,3 +1,31 @@
+use std::process::Command;
+
+enum LoopControl {
+    CONTINUE,
+    STOP,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut clear = Command::new("clear");
+    let _clear_status = clear.status().expect("Failed to clear the terminal.");
+    println!("---------------------------------------------------------------");
+    println!("\tWelcome to the Sentence Data Aggregator.");
+    println!("---------------------------------------------------------------");
+
+    let mut l = LoopControl::CONTINUE;
+    while match l {
+        LoopControl::CONTINUE => true,
+        LoopControl::STOP => false,
+    } {
+        l = input_loop();
+    }
+
+    println!("---------------------------------------------------------------");
+    println!("\t\tThank you and goodbye.");
+    println!("---------------------------------------------------------------");
+}
+
+// -----------------------------------------------------------------------------
+fn input_loop() -> LoopControl {
+    return LoopControl::CONTINUE;
 }
