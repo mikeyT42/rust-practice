69499076423ad5ceed648e605de66614c0b9d2a9
mikeyT42 <mrmojorisen0@gmail.com>
2024-08-27 22:26:56 -0400

    Got validation to compile.

--- sums-and-averages/src/main.rs
+++ sums-and-averages/src/main.rs
@@ -1,4 +1,35 @@
-use std::process::Command;
+use std::{io, num::ParseFloatError, process::Command};
+
+const SENTINEL: &'static str = "\n";
+
+enum ValidationError {
+    Parse(ParseFloatError),
+    NoInput,
+}
+
+impl From<ParseFloatError> for ValidationError {
+    fn from(error: ParseFloatError) -> ValidationError {
+        ValidationError::Parse(error)
+    }
+}
+
+struct Sums {
+    positive: u64,
+    negative: i32,
+    overall: i32,
+}
+
+struct Counts {
+    positive: u8,
+    negative: u8,
+    overall: u8,
+}
+
+struct Averages {
+    positive: f64,
+    negative: f64,
+    overall: f64,
+}
 
 enum LoopControl {
     CONTINUE,
@@ -27,5 +58,50 @@ fn main() {
 
 // -----------------------------------------------------------------------------
 fn input_loop() -> LoopControl {
+    println!(
+        "Please input up to floating point or integer numbers. Seperate\n\
+      them with spaces. Simply enter a newline character to exit.\n"
+    );
+
+    let mut input = String::new();
+    if let Err(e) = io::stdin().read_line(&mut input) {
+        eprintln!("Could not read from stdin:: {}", e);
+        return LoopControl::STOP;
+    }
+    if input == SENTINEL {
+        return LoopControl::STOP;
+    }
+
+    let numbers = match validate(&input) {
+        Ok(numbers) => numbers,
+        Err(ValidationError::Parse(parse_float_error)) => {
+            eprintln!(
+                "There was an error parsing your input:: \n\
+                {}",
+                parse_float_error
+            );
+            return LoopControl::CONTINUE;
+        }
+        Err(ValidationError::NoInput) => {
+            eprintln!("There was no input provided\n");
+            return LoopControl::CONTINUE;
+        }
+    };
+
     return LoopControl::CONTINUE;
+}
+
+// -----------------------------------------------------------------------------
+fn validate(input: &str) -> Result<Vec<f64>, ValidationError> {
+    let parsed_input_numbers = input
+        .lines()
+        .next()
+        .ok_or_else(|| ValidationError::NoInput)?
+        .split_whitespace()
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<f64>())
+        .collect::<Result<Vec<f64>, ParseFloatError>>()?;
+
+    return Ok(parsed_input_numbers);
 }
